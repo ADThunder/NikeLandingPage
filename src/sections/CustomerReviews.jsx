d65334bfb069d6011ffe9b9fefda515120da31d9
ADThunder <maianhdung02051996@gmail.com>
2023-12-02 00:39:52 +0700

    day code

---
+++ src/sections/CustomerReviews.jsx
@@ -0,0 +1,31 @@
+import ReviewCard from "../Components/ReviewCard";
+import { reviews } from "../contants";
+
+const CustomerReviews = () => {
+  return (
+    <section className="max-container ">
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="ml-3 text-coral-red">Customers</span> Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+        Hear genuine stories from out satisfied customers about their
+        exceptional experiences with us.
+      </p>
+
+      <div className="mt-24 flex flex-1 justify-evenly item-centers max-lg:flex-col gap-14">
+        {reviews.map((review) => (
+          <ReviewCard
+            key={review.customerName}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default CustomerReviews;
